6463429f7f222a4d7799627dfd59b4c65c5bb5a4
Dai MIKURUBE <dmikurube@treasure-data.com>
2018-12-20 16:16:31 +0900

    Disable plugin crawling from GitHub and RubyGems

--- server.rb
+++ server.rb
@@ -1,24 +1,8 @@
 require 'sinatra'
 require 'sinatra/streaming'
-require_relative 'render'
 
 set :server, 'puma'
 set :port, (ENV['PORT'] || 8580).to_i
-
-get '/update' do
-  headers 'Content-Type' => 'text/plain'
-  stream do |out|
-    begin
-      update_index(Logger.new(out))
-    rescue => e
-      out.puts e.to_s
-      e.backtrace.each {|bt|
-        out.write "  "
-        out.puts bt
-      }
-    end
-  end
-end
 
 get '/' do
   headers "Content-Type" => ""
